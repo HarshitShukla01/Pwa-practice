8729afc597f80713a75603df36cfa943decd156d
HarshitShukla01 <harshitshukla258@gmail.com>
2021-10-16 12:13:11 +0530

    initial commit

---
+++ myscript.js
@@ -0,0 +1,12 @@
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("./service-worker.js");
+}
+
+// "start_url": "/?home=true",
+/*
+ ,
+  "theme_color": "#000000",
+  "background_color": "#FFFFFF",
+  "display": "fullscreen",
+  "orientation": "portrait"
+ */
